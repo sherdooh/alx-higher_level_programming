57b997319ee4afc8cccdaa76490bf2296c0c199c
sherdooh <sherkiplah@gmail.com>
2023-07-03 00:39:59 -0700

    bodysieze

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-
-# Check if a URL is provided as an argument
-if [[ $# -ne 1 ]]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Store the URL from the command-line argument
-url=$1
-
-# Send the request and get the size of the response body
-response=$(curl -s -w "%{size_download}" -o /dev/null "$url")
-
-# Display the size of the response body
-echo "Size of the response body: $response bytes"
-
+curl -sI "$1" | awk '/Content-Length/ {print $2}' | tr -d '\r'
