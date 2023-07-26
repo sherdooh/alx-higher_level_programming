8af7ce08466b5edb048aaf091274a165eb7cba6a
Sherdooh <Sherkiplah@gmail.com>
2023-07-26 19:47:04 +0000

    request store

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+t fs = require('fs');
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
+
